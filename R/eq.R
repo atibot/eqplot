3d5f00efec5cd9edd588b92ab7236217cd419d93
Hideh Tobita <h-tobita@tkc.att.ne.jp>
2018-01-04 23:39:37 +0900

    correct version 0.1.1 again

--- R/eq.R
+++ R/eq.R
@@ -198,11 +198,15 @@ eq_location_clean <- function(x) {
 #'
 #' @examples \dontrun{
 #' library(dplyr)
+#' library(lubridate)
 #' data(raw_df)
-#' raw_df %>% eq_clean_data() %>%
+#' test_df <- raw_df %>% eq_clean_data() %>%
 #' filter(COUNTRY == "TURKEY" | COUNTRY == "JAPAN") %>%
-#' dplyr::filter(DATE >= eq_date("2000-01-01") & DATE <= eq_date("2017-01-01")) %>%
-#' ggplot2::ggplot() +
+#' dplyr::filter(DATE >= eq_date("2000-01-01") & DATE <= eq_date("2017-01-01"))
+#' }
+#'
+#' @examples \dontrun{
+#' ggtimeline <- ggplot2::ggplot(test_df) +
 #' geom_timeline(ggplot2::aes(
 #'                            x = DATE,
 #'                            y = COUNTRY,
@@ -212,7 +216,9 @@ eq_location_clean <- function(x) {
 #' eq_theme +
 #' ggplot2::scale_size_continuous(name = "Richer scale value", breaks = c(0, 2, 4, 6, 8)) +
 #' ggplot2::scale_fill_continuous(name = "# deaths") +
-#' ggplot2::labs("Earthquakes")
+#' ggplot2::ggtitle("Earthquakes")
+#'
+#' ggtimeline
 #' }
 #'
 #' @export
@@ -349,7 +355,7 @@ eq_theme <-  ggplot2::theme_minimal() + ggplot2::theme(
 #' @importFrom grid gpar segmentsGrob textGrob gTree gList
 #'
 #' @examples \dontrun{
-#' ggtimeline2 + geom_timeline_label(ggplot2::aes(
+#' ggtimeline + geom_timeline_label(ggplot2::aes(
 #'                                   x = DATE,
 #'                                   y = COUNTRY,
 #'                                   mag = EQ_PRIMARY,
